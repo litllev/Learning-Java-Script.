0358fb31dbf9cde46830cbbb32c0ec0accf0efd6
litllev <vitorherisonvhp@gmail.com>
2023-10-18 21:12:40 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// exercicio 1
+console.log("hello world")
+
+// exercicio 2 conversão de numero
+
+const inicial = "1234";
+console.log(typeof Number(inicial))
+
+// exercicio 3 manipulação de strings
+
+let js = "javaScript é incrivel";
+
+console.log('O total de caracteres na frase "javaScript é incrivel" é: '+ (js.length));
+console.log('O total de palavras na frase "javaScript é incrivel" é: '+(js.split(' ')).length);
+
+// exercicio 4 Loops
+
+var listaEx4 = ['João','Maria','Flavio','Vito'];
+
+for (var i=0; i <= listaEx4.length;i++) {
+    console.log(listaEx4[i])
+}
+

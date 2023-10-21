57565a04db0d2d5effeac5e6125e5d9e91dcbbc1
litllev <vitorherisonvhp@gmail.com>
2023-10-20 22:38:52 -0300

    Projeto carrinho inicio

---
+++ projeto1/script.js
@@ -0,0 +1,13 @@
+function soma(value) {
+    var preco = parseFloat(document.getElementById('preco').textContent)
+    var qtd = document.getElementById('qtd').textContent
+    var total = preco * qtd;
+    console.log(qtd.textContent);
+    console.log(qtd);
+    console.log(total);
+    if (value === 1){
+       
+
+    }
+
+}
